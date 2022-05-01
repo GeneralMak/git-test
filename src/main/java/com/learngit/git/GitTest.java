2cf94b1d31a2796afb0c12eb89c97735075d949f
mark <3174228761@qq.com>
2022-05-01 17:13:39 +0800

    master commit2

--- src/main/java/com/learngit/git/GitTest.java
+++ src/main/java/com/learngit/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello hot-fix git4");
+        System.out.println("hello master conflict test");
+
     }
 }
