ad5bbcbb39e5baffa1bfe59e896597d204ac441f
mark <3174228761@qq.com>
2022-05-02 09:18:08 +0800

    push commit

--- src/main/java/com/learngit/git/GitTest.java
+++ src/main/java/com/learngit/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master conflict test");
 
         System.out.println("hello hot-fix conflict test");
+        System.out.println("push test");
     }
 }
